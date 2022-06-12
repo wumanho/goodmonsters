a6e659a2d4c730bd5abbab1480b602ec9a3586a9
WUMANHO <wumanho@tutanota.com>
2022-06-12 15:19:05 +0800

    for and filter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
 import {Component} from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      name: 'wumanho',
-      company:'GD'
+      monsters: [],
+      searchField: ''
     }
+    console.log('constructor')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(resJson => {
+        this.setState(() => {
+          return {monsters: resJson}
+        })
+      })
+  }
+
+  onSearchChange = (e) => {
+    this.setState(() => {
+      return {searchField: e.target.value.toLowerCase()}
+    })
   }
 
   render() {
+    console.log('render')
+    const {monsters, searchField} = this.state
+    const {onSearchChange} = this
+
+    const displayList = monsters.filter(m => {
+      const lower = m.name.toLowerCase()
+      return lower.includes(searchField)
+    })
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>Hi {this.state.name} I'm working on {this.state.company}</p>
-          <button onClick={()=>{
-            this.setState({name:'oh my gosh'})
-          }}>
-            Change Name
-          </button>
-        </header>
-      </div>
-    );
+        <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}/>
+        {
+          displayList.map(m => {
+            return <h1 key={m.id}>{m.name}</h1>
+          })
+        }
+      </div>)
   }
 }
 
